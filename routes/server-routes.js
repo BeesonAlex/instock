6d52aa2cb75f23ea8ddcfba6c1f8bc4c24a6459c
Alex Beeson <beeson.alexander@gmail.com>
2019-05-23 14:04:21 -0400

    Completed POST Requests for creating new inventory and warehouses

--- routes/server-routes.js
+++ routes/server-routes.js
@@ -6,13 +6,64 @@ const getData = (req, res) => {
     res.json(data)
 }
 
+let inventoryId = 8;
+const createInventoryId = () => {
+    return inventoryId++;
+}
+
+let warehouseId = 5;
+const createWarehouseId = () => {
+   return warehouseId++;
+}
+
+const createNewInventoryItem = (req, res) => {
+    const {itemName, lastOrdered, city, country, quantity, status, itemDescription} = req.body;
+    const newProduct = {
+        id: createInventoryId(),
+        productName: itemName,
+        itemDescription,
+        quantity,
+        lastOrdered,
+        location: `${city}, ${country}`,
+        status,
+        categories,
+    };
+    inventoryArray.push(newProduct);
+    res.json(newProduct);
+}
+
+
+const createNewWarehouse = (req, res) => {
+    const {warehouseName, street, suiteNum, city, province, postal, contactName, title, phone, email} = req.body;
+    const newWarehouse = {
+        id: createWarehouseId(),
+        warehouseName,
+        address: {
+            street,
+            suiteNum,
+            city,
+            province,
+            postal,
+        },
+        contact: {
+            contactName,
+            position: title,
+            phone,
+            email,
+        },
+        inventoryCategories,
+    };
+    warehouseArray.push(newWarehouse);
+    res.json(newWarehouse);
+}
+
 app.route('/data')
     .get(getData);
 
 
-router.get('/', getAllVideos);
-router.post('/', uploadVideo);
-router.post('/:id/comments/', postComment);
+router.get('/', getData);
+router.post('/inventory',createNewInventoryItem);
+router.post('/warehouses', createNewWarehouse);
 router.get('/:id', getSingleVideo);
 router.delete('/:id/comments/:commentId', deleteComment);
 
