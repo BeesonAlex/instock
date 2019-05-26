df0c6d9990b9f0b5e3fd27f2588b213689d023b7
Danbi Kim <danbikim@danbis-air.lan>
2019-05-26 15:31:08 -0400

    navbar font family and font color updated

--- client/src/components/inventory/Inventory.js
+++ client/src/components/inventory/Inventory.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import searchIcon from '../../assets/Icons/SVG/Icon-search.svg'
 import InventoryItem from './InventoryItem'
+import './inventory.scss';
 
 class Inventory extends React.Component {
     render() {
-        console.log(this.props.inventory)
         return (
-            <>
+            <section className="inventoryList">
                 <div className="header">
                     <h2 className="header__title">Inventory</h2>
                     <div className="header__search">
                         <img className="header__search--searchIcon" src={searchIcon} alt="search icon"/>
-                        <input className="header__search--input" placeholder="search" />
+                        <input className="header__search--input" placeholder="Search" />
                     </div>
                     
                 </div>
                 <div className="subheadings">
-                    <h3 className="product__item">ITEM</h3>
-                    <h3 className="product__lastOrdered">LAST ORDERED</h3>
-                    <h3 className="product__location">LOCATION</h3>
-                    <h3 className="product__quantity">QUANTITY</h3>
-                    <h3 className="product__status">STATUS</h3>
+                    <h3 className="product__item product__heading">ITEM</h3>
+                    <h3 className="product__lastOrdered product__heading">LAST ORDERED</h3>
+                    <h3 className="product__location product__heading">LOCATION</h3>
+                    <h3 className="product__quantity product__heading">QUANTITY</h3>
+                    <h3 className="product__status product__heading">STATUS</h3>
                 </div>
                 
                 <section className="inventoryItems">
@@ -30,7 +30,7 @@ class Inventory extends React.Component {
                         })
                     }
                 </section>
-            </>
+            </section>
         )
     }
 }
--- client/src/components/inventory/InventoryItem.js
+++ client/src/components/inventory/InventoryItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './inventory.scss';
 
 class InventoryItem extends React.Component {
     render() {
         const { name, description, lastOrdered, location, quantity, isInstock } = this.props.product;
-        console.log(this.props.product)
         return (
             <div className="product">
-                <h3 className="product__item">ITEM</h3>
+                <h3 className="product__item product__heading">ITEM</h3>
                 <div className="product__detail">
                     <h3 className="product__name">{name}</h3>
                     <p className="product__description product__detail">{description}</p>
