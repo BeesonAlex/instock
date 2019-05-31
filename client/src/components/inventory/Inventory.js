4199bd8e6f532fcd1dc18cef8636b210d10cb119
DuskisNigh <s.ashmcc@gmail.com>
2019-05-31 17:20:54 -0400

    implemented inventory post

--- client/src/components/inventory/Inventory.js
+++ client/src/components/inventory/Inventory.js
@@ -25,11 +25,36 @@ class Inventory extends React.Component {
 			.then(response => {
 			const inventory = response.data.inventoryArray;
 			this.setState({inventory});
-			console.log(inventory)
 			})
 			.catch(error => {
 			console.log(error)
       })
+	};
+
+	onSubmitHandler = event => {
+		event.preventDefault();
+		console.log(event.target);
+		const postInfo = {
+			name: event.target.product_input.value,
+			lastOrdered: event.target.order_date_input.value,
+			location: event.target.city_input.value & ', ' & event.target.country_dropdown,
+			quantity: event.target.quantity_input.value,
+			description: event.target.item_description.value
+		}
+
+		const newItem = axios
+			.post(`http://localhost:8080/data/inventory/`, postInfo)
+			.then(res => {
+				axios
+					.get(`http://localhost:8080/data/inventory/`)
+					.then(res => {
+						console.log(res.data);
+						this.setState({
+							inventory: [...this.state.inventory, newItem]
+						});
+					});
+			});
+		this.closeModalHandler();
 	};
 	
 	openModalHandler = () => {
@@ -56,7 +81,7 @@ class Inventory extends React.Component {
             <div className="header">
                <h2 className="header__title">Inventory</h2>
                <div className="header__search">
-                  <img className="header__search--searchIcon" src={searchIcon} alt="search icon"/>
+                  <img className="header__search--searchIcon" src={searchIcon} alt="search icon" />
                   <input className="header__search--input" placeholder="Search" />
                </div>
             </div>
@@ -77,25 +102,25 @@ class Inventory extends React.Component {
 							show={this.state.isShowing}
 							close={this.closeModalHandler}>
 							<div className="main-modal-div">
-								<form className="modal-form" id="modal-form">
+								<form className="modal-form" id="modal-form" onSubmit={this.onSubmitHandler}>
 									<div className="first-form-div">
 										<div className="product-div">
 											<label className="product-label" htmlFor="product-input">PRODUCT</label>
-											<input className="product-input" type="text" name="product-input" id="product-input" placeholder="Item Name" />
+											<input className="product-input" type="text" name="product_input" placeholder="Item Name" />
 										</div>
 										<div className="last-order-div">
 											<label className="order-date-label" htmlFor="order-date-input">LAST ORDERED</label>
-											<input className="order-date-input" type="text" name="order-date-input" id="order-date-input" placeholder="yyyy-mm-dd" />
+											<input className="order-date-input" type="text" name="order_date_input" placeholder="yyyy-mm-dd" />
 										</div>
 									</div>
 									<div className="second-form-div">
 										<div className="city-div">
 											<label className="city-label" htmlFor="city-input">CITY</label>
-											<input className="city-input" type="text" name="city-input" id="city-input" placeholder="City" />
+											<input className="city-input" type="text" name="city_input" placeholder="City" />
 										</div>
 										<div className="country-div">
 											<label className="country-label" htmlFor="country-dropdown">COUNTRY</label>
-											<select className="country-dropdown" name="country-dropdown" id="country-dropdown">
+											<select className="country-dropdown" name="country_dropdown">
 												<option value="Canada">Canada</option>
 												<option value="USA">USA</option>
 												<option value="Mexico">Mexico</option>
@@ -105,7 +130,7 @@ class Inventory extends React.Component {
 									<div className="third-form-div">
 										<div className="quantity-div">
 											<label className="quantity-label" htmlFor="quantity-input">QUANTITY</label>
-											<input className="quantity-input" type="text" name="quantity-input" id="quantity-input" placeholder="0" />
+											<input className="quantity-input" type="text" name="quantity_input" placeholder="0" />
 										</div>
 										<div className="status-div">
 											<label className="status-label" htmlFor="status-switch">STATUS</label>
@@ -119,7 +144,15 @@ class Inventory extends React.Component {
 									</div>
 									<div className="fourth-form-div">
 										<label className="item-label" htmlFor="item-description">ITEM DESCRIPTION</label>
-										<textarea className="item-description" name="item-description" id="item-description" cols="30" rows="10" placeholder="(Optional)"></textarea>
+										<textarea className="item-description" name="item_description" cols="30" rows="10" placeholder="(Optional)"></textarea>
+									</div>
+									<div className="modal-footer">
+										<button className="btn-cancel" onClick={this.close}>
+											CANCEL
+          							</button>
+										<button type="submit" className="btn-save">
+											SAVE
+          							</button>
 									</div>
 								</form>
 							</div>
