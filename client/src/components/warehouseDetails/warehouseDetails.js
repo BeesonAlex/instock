89521f8c3f9c858ece58a875ff38547933e2df26
root <root@LAPTOP-HBCTG0EI.localdomain>
2019-06-02 10:45:10 -0400

    committing before pulling to update to current

--- client/src/components/warehouseDetails/warehouseDetails.js
+++ client/src/components/warehouseDetails/warehouseDetails.js
@@ -1,42 +1,99 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './warehouseDetails.scss';
 import backarrow from '../../assets/Icons/SVG/Icon-back-arrow.svg';
-import Inventory from '../inventory/Inventory';
+import InventoryItem from '../inventory/InventoryItem';
+import axios from 'axios';
 
 class WarehouseDetails extends Component {
+
+    state = {
+        locationDetails: {
+            name: '',
+            address: {
+                city: '',
+                postal: '',
+                province: '',
+                street: '',
+                suiteNum: ''
+            },
+            contact: {
+                email: '',
+                name: '',
+                phone: '',
+                title: '',
+            },
+            id: '',
+            inventoryCategories: ''
+        },
+        allInventory: [],
+        localInventory: []
+    };
+
+    componentDidMount() {
+
+        const { id } = this.props.match.params;
+
+        axios.get(`http://localhost:8080/data/inventory/`)
+            .then(response => {
+                const allInventory = response.data.inventoryArray;
+                this.setState({allInventory});
+                const localInventory = allInventory.filter((inventory) => {
+                    return (inventory.warehouseId === this.props.match.params.id)
+                })
+                this.setState({localInventory});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        axios.get(`http://localhost:8080/data/warehouses/${id}`)
+            .then(response => {
+                const locationDetails = response.data.targetWarehouse;
+                this.setState({locationDetails});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
     render() {
         return (
             <>
             <section className="warehouseDetails">
                 <h1 className="warehouseDetails__title">
-                    <img className="warehouseDetails__backArrow" src={backarrow} alt="back" />
-                    Warehouse Name
+                    <Link className="warehouseDetails__backArrow--link" to={"/warehouses"}><img className="warehouseDetails__backArrow" src={backarrow} alt="back" /></Link>
+                    {this.state.locationDetails.name}
                 </h1>
-                <div className="warehouseDetails__address">
-                    <h4 className="warehouseDetails__address--label">ADDRESS</h4>
-                    <div className="warehouseDetails__street-container">
-                        <h3 className="warehouseDetails__address--street">123 Main Street</h3>
-                        <h3 className="warehouseDetails__address--suite">Suite 201</h3>
+                <div className="warehouseDetails__divider" />
+                <div className="warehouseDetails-container">
+                    <div className="warehouseDetails__address">
+                        <h4 className="warehouseDetails__address--label">ADDRESS</h4>
+                        <div className="warehouseDetails__street-container">
+                            <h3 className="warehouseDetails__address--street">{this.state.locationDetails.address.street}</h3>
+                            <h3 className="warehouseDetails__address--suite">{this.state.locationDetails.address.suiteNum}</h3>
+                        </div>
+                        <div className="warehouseDetails__city-container">
+                            <h3 className="warehouseDetails__address--city">{this.state.locationDetails.address.city}, ON</h3>
+                            <h3 className="warehouseDetails__address--postal-code">{this.state.locationDetails.address.postal} CA</h3>
+                        </div>
                     </div>
-                    <div className="warehouseDetails__city-container">
-                        <h3 className="warehouseDetails__address--city">Toronto, ON</h3>
-                        <h3 className="warehouseDetails__address--postal-code">M65GB7 CA</h3>
-                    </div>
-                </div>
-                <div className="warehouseDetails__contact">
-                    <h4 className="warehouseDetails__contact--label">CONTACT</h4>
-                    <div className="warehouseDetails-contact-container">
-                        <h3 className="warehouseDetails__contact--name">Mara Weinberg</h3>
-                        <h3 className="warehouseDetails__contact--job-title">Warehouse Manager</h3>
-                    </div>
-                    <div className="warehouseDetails__phone-container">
-                        <h3 className="warehouseDetails__contact--phone">+1 416 678 2345</h3>
-                        <h3 className="warehouseDetails__contact--email">weinberg@instock.com</h3>
+                    <div className="warehouseDetails__contact">
+                        <h4 className="warehouseDetails__contact--label">CONTACT</h4>
+                        <div className="warehouseDetails-contact-container">
+                            <h3 className="warehouseDetails__contact--name">{this.state.locationDetails.contact.name}</h3>
+                            <h3 className="warehouseDetails__contact--job-title">{this.state.locationDetails.contact.title}</h3>
+                        </div>
+                        <div className="warehouseDetails__phone-container">
+                            <h3 className="warehouseDetails__contact--phone">+1 {this.state.locationDetails.contact.phone}</h3>
+                            <h3 className="warehouseDetails__contact--email">{this.state.locationDetails.contact.email}</h3>
+                        </div>
                     </div>
                 </div>
             </section>
             <section className="inventoryDetails">
-                <Inventory />
+                {this.state.localInventory.map((localInventory, id) => {
+                    return <InventoryItem className="inventoryItem" key={id} product={localInventory} removeItem={this.removeItem}/>})
+                }
             </section>
             </>
         )
